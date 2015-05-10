719577e475e8a86a5800def4fc14127b5b55760c
Keith Tucker <tuck220@icloud.com>
2015-05-10 08:18:23 -0400

    Initial release
    Ansible playbook for configuring the midas application. This release
    deploys on Linux distributions based on either Debian or Red Hat.

---
+++ secrets.yml.ex
@@ -0,0 +1,15 @@
+---
+# Example variables file for the sensitive parameters.
+# Rename this to secrets.yml and edit to make it unique for your instance.
+# The git repository is set to ignore the secrets.yml file, so it won't
+# inadvertently copied into the repo and shared with others.
+midas_database_password: midas
+
+# Amazon Web Services credentials
+#aws_access_key:
+#aws_secret_key:
+
+# Private key to use for DomainKeys Identified Mail.
+# Default is Javascript code to dynamically read the private key at runtime.
+#midas_dkim_private_key: fs.readFileSync("private_key.pem")
+
